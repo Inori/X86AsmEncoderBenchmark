465db265ba69977dcece0bab292e534362ec7325
Asuka <bingkan@outlook.com>
2022-02-24 17:04:36 +0800

    Add Zydis test.

--- X86AsmEncoderBenchmark/main.cpp
+++ X86AsmEncoderBenchmark/main.cpp
@@ -11,7 +11,9 @@
 
 #define TEST_LOOP_COUNT 10000
 
-void BenchAsmjit(asmjit::x86::Assembler& a)
+volatile unsigned char data = 0;
+
+void TestAsmjit(asmjit::x86::Assembler& a)
 {
 	using namespace asmjit;
 	using namespace asmjit::x86;
@@ -51,7 +53,7 @@ void BenchAsmjit(asmjit::x86::Assembler& a)
 	a.pop(rax);
 }
 
-void TestAsmjit()
+void BenchAsmjit()
 {
 	using namespace asmjit;
 
@@ -67,11 +69,14 @@ void TestAsmjit()
 		code.init(rt.environment());
 		code.attach(&a);
 
-		BenchAsmjit(a);
+		TestAsmjit(a);
 		
 		CodeBuffer& buffer = code.sectionById(0)->buffer();
-		const void* p = buffer.data();
+		const unsigned char* p = buffer.data();
 		size_t s = buffer.size();
+
+		// avoid to be optimized
+		data = *p;
 
 		code.detach(&a);
 		code.reset();
@@ -82,7 +87,7 @@ void TestAsmjit()
 	std::cout << "Asmjit " << elapsed.count() << " ms\n";
 }
 
-void BenchXbyak(Xbyak::CodeGenerator& a)
+void TestXbyak(Xbyak::CodeGenerator& a)
 {
 	using namespace Xbyak::util;
 
@@ -121,7 +126,7 @@ void BenchXbyak(Xbyak::CodeGenerator& a)
 	a.pop(rax);
 }
 
-void TestXbyak()
+void BenchXbyak()
 {
 	using namespace Xbyak::util;
 
@@ -131,10 +136,13 @@ void TestXbyak()
 
 	for (size_t i = 0; i != TEST_LOOP_COUNT; ++i)
 	{
-		BenchXbyak(code);
+		TestXbyak(code);
 
-		const void* p = code.getCode();
+		const unsigned char* p = code.getCode();
 		size_t s = code.getSize();
+
+		// avoid to be optimized
+		data = *p;
 
 		code.reset();
 	}
@@ -147,7 +155,7 @@ void TestXbyak()
 
 }
 
-void BenchZasm(zasm::Assembler& a)
+void TestZasm(zasm::Assembler& a)
 {
 	using namespace zasm;
 	using namespace zasm::operands;
@@ -187,7 +195,7 @@ void BenchZasm(zasm::Assembler& a)
 	a.pop(rax);
 }
 
-void TestZasm()
+void BenchZasm()
 {
 	using namespace zasm;
 	using namespace zasm::operands;
@@ -197,17 +205,24 @@ void TestZasm()
 
 	auto start = std::chrono::high_resolution_clock::now();
 
-
+	int64_t address = 0x00400000;
 	for (size_t i = 0; i != TEST_LOOP_COUNT; ++i)
 	{
-		BenchZasm(assembler);
+		TestZasm(assembler);
 
 		// Encodes all the nodes.
-		program.serialize(0x00400000);
+		
+		program.serialize(address);
+		address += 0x10;
 
-		const void* p = program.getCode();
+		const unsigned char* p = program.getCode();
 		size_t s = program.getCodeSize();
 
+		// here p is null the second loop
+		 
+		
+		// avoid to be optimized
+		// data = *p;
 
 		program.clear();
 	}
@@ -218,13 +233,116 @@ void TestZasm()
 }
 
 
+#define PUSH_POP_REG(reg_name) \
+		inst_size = buffer_size - cur_pos; \
+		req.operands[0].reg.value = ZYDIS_REGISTER_##reg_name; \
+		ZydisEncoderEncodeInstruction(&req, buffer.data() + cur_pos, &inst_size); \
+		cur_pos += inst_size;
+
+size_t TestZydis(std::vector<unsigned char>& buffer)
+{
+	size_t cur_pos = 0;
+	size_t buffer_size = buffer.size();
+	size_t inst_size = buffer_size;
+
+	ZydisEncoderRequest req;
+	memset(&req, 0, sizeof(req));
+	req.machine_mode = ZYDIS_MACHINE_MODE_LONG_64;
+
+	// begin push
+	req.mnemonic = ZYDIS_MNEMONIC_PUSH;
+	req.operand_count = 1;
+	req.operands[0].type = ZYDIS_OPERAND_TYPE_REGISTER;
+
+	PUSH_POP_REG(RAX);
+	PUSH_POP_REG(RCX);
+	PUSH_POP_REG(RDX);
+	PUSH_POP_REG(RBX);
+	PUSH_POP_REG(RBP);
+	PUSH_POP_REG(RSI);
+	PUSH_POP_REG(RDI);
+	PUSH_POP_REG(R8);
+	PUSH_POP_REG(R9);
+	PUSH_POP_REG(R10);
+	PUSH_POP_REG(R11);
+	PUSH_POP_REG(R12);
+	PUSH_POP_REG(R13);
+	PUSH_POP_REG(R14);
+	PUSH_POP_REG(R15);
+
+	// pushfq
+	req.mnemonic = ZYDIS_MNEMONIC_PUSHFQ;
+	req.operand_count = 0;
+
+	inst_size = buffer_size - cur_pos;
+	ZydisEncoderEncodeInstruction(&req, buffer.data() + cur_pos, &inst_size);
+	cur_pos += inst_size;
+
+	// popfq
+	req.mnemonic = ZYDIS_MNEMONIC_POPFQ;
+	req.operand_count = 0;
+
+	inst_size = buffer_size - cur_pos;
+	ZydisEncoderEncodeInstruction(&req, buffer.data() + cur_pos, &inst_size);
+	cur_pos += inst_size;
+
+
+	// begin pop
+	req.mnemonic = ZYDIS_MNEMONIC_POP;
+	req.operand_count = 1;
+	req.operands[0].type = ZYDIS_OPERAND_TYPE_REGISTER;
+
+	PUSH_POP_REG(R15);
+	PUSH_POP_REG(R14);
+	PUSH_POP_REG(R13);
+	PUSH_POP_REG(R12);
+	PUSH_POP_REG(R11);
+	PUSH_POP_REG(R10);
+	PUSH_POP_REG(R9);
+	PUSH_POP_REG(R8);
+	PUSH_POP_REG(RDI);
+	PUSH_POP_REG(RSI);
+	PUSH_POP_REG(RBP);
+	PUSH_POP_REG(RBX);
+	PUSH_POP_REG(RDX);
+	PUSH_POP_REG(RCX);
+	PUSH_POP_REG(RAX);
+
+	return cur_pos;
+}
+
+void BenchZydis()
+{
+	auto start = std::chrono::high_resolution_clock::now();
+
+	std::vector<unsigned char> code_buffer;
+	code_buffer.resize(0x1000);
+
+	for (size_t i = 0; i != TEST_LOOP_COUNT; ++i)
+	{
+		size_t s = TestZydis(code_buffer);
+
+		const unsigned char* p = code_buffer.data();
+
+		// avoid to be optimized
+		data = *p;
+	}
+
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> elapsed = end - start;
+	std::cout << "Zydis " << elapsed.count() << " ms\n";
+}
+
+
 int main()
 {
-	TestAsmjit();
+	BenchAsmjit();
 
-	TestXbyak();
+	BenchXbyak();
 
-	TestZasm();
+	BenchZydis();
+
+	BenchZasm();
 
 	getchar();
 	return 0;
@@ -232,20 +350,22 @@ int main()
 
 /*
 
-CPU: Intel i7-9750H
+CPU: Intel i7-8700
 Average output for test several times:
 
 Debug Build:
 
-Asmjit 79.5586 ms
-Xbyak 268.761 ms
-Zasm 34107.5 ms
+Asmjit 77.5361 ms
+Xbyak 206.549 ms
+Zydis 88.2653 ms
+Zasm 31610.4 ms
 
 
 Release Build:
 
-Asmjit 7.0578 ms
-Xbyak 13.9802 ms
-Zasm 6339.41 ms
+Asmjit 6.3319 ms
+Xbyak 11.3565 ms
+Zydis 25.2644 ms
+Zasm 6156.57 ms
 
 */
